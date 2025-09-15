f45957464721102bffd8895d0d4728da80c29157
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-09-15 12:41:07 +0200

    [#83175] views: icons: check: Add prop to define size of the icon
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- src/views/icons/check.tsx
+++ src/views/icons/check.tsx
@@ -8,12 +8,13 @@
 
 interface CheckIconProps {
   color?: string,
+  size?: string,
 }
 
 
-export default function CheckIcon({color = "#D5D5D5"}: CheckIconProps) {
+export default function CheckIcon({color = "#D5D5D5", size = "16"}: CheckIconProps) {
   return (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M13.3333 4L5.99996 11.3333L2.66663 8" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
   );
