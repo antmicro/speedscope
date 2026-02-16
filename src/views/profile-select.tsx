a8be8ce834ba1071fdbc09900f922c4de638f8bd
Andrii Chmutov <achmutov@antmicro.com>
2026-02-16 14:22:33 +0100

    [#90388] src: views: profile-select: Close on focus out
    Signed-off-by: Andrii Chmutov <achmutov@antmicro.com>

--- src/views/profile-select.tsx
+++ src/views/profile-select.tsx
@@ -320,6 +320,7 @@ export function ProfileSelect({
             onKeyUp={stopPropagation}
             onKeyPress={stopPropagation}
             onFocus={stopPropagation}
+            onFocusOut={closeProfileSelect}
           />
         </div>
         <div className={css(style.filterBorder)} />
